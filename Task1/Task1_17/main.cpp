cf36d7170ec3ec9da8193d58aa6a4e47e0e66260
SidenkoVitaliy <vitalk_0@qip.ru>
2012-06-06 18:24:55 +0300

    new task added

---
+++ Task1/Task1_17/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+unsigned long subAbs(long, long);
+
+int main()
+{
+	long number1, number2;
+
+	cout << "Enter two numbers:" << endl;
+	cin >> number1 >> number2;
+	cout << "Result:" << endl << subAbs(number1, number2) << endl;
+
+	return 0;
+}
+
+unsigned long subAbs(long num1, long num2)
+{
+	unsigned long result = num1 - num2;
+
+	// Get sign bit from result
+	unsigned long signBit = (result >> 31) & 1;
+
+	// Inverse and increment result if sign bit is set
+	result = (result ^ (~signBit + 1)) + signBit;
+
+	return result;
+}
