91393efd3d4aeaf0409aeebbed3de671846e0262
SidenkoVitaliy <vitalk_0@qip.ru>
2012-06-16 00:02:06 +0300

    task 3 var 15 finished

--- Task3/Task3_15/main.cpp
+++ Task3/Task3_15/main.cpp
@@ -1,4 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+void hideWords(char *str, char c);
+
 int main()
 {
+	char str[200];
+	cin.getline(str, 200);
+	hideWords(str, 's');
+	cout << str;
 	return 0;
+}
+
+void hideWords(char *pStr, char c)
+{
+	if(!pStr)
+		return;
+
+	while(*pStr)
+	{
+		if(*pStr != ' ')
+		{
+			char *pStart = pStr;
+			bool hide = false;
+
+			while(*pStr && (*pStr != ' '))
+			{
+				if(*pStr == c)
+					hide = true;
+
+				pStr++;
+			}
+			
+			if(hide)
+				memset(pStart, '*', pStr - pStart); 
+		}
+
+		pStr++;
+	}
 }
