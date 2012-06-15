ac22dc39360152c5a25c9d37775c80b9284a5d19
SidenkoVitaliy <vitalk_0@qip.ru>
2012-06-16 00:09:56 +0300

    user input added (task3(15))

--- Task3/Task3_15/main.cpp
+++ Task3/Task3_15/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#define MAX_LEN 200
 
 using namespace std;
 
@@ -6,10 +7,27 @@ void hideWords(char *str, char c);
 
 int main()
 {
-	char str[200];
-	cin.getline(str, 200);
-	hideWords(str, 's');
-	cout << str;
+	char *pStrBuf = new char[MAX_LEN];
+	char c;
+
+	for(;;)
+	{
+		
+		cout << "Input sentence (\"e\" to exit): " << endl;
+		cin.getline(pStrBuf, 200);
+
+		if(!strcmp(pStrBuf, "e"))
+			break;
+
+		cout << "Input character: " << endl;
+		cin >> c;
+
+		hideWords(pStrBuf, c);
+
+		cout << "Result: " << endl << pStrBuf << endl;
+	}
+
+	delete[] pStrBuf;
 	return 0;
 }
 
