c0a863ebdd91fafcd0b2caf70c5f50241ea5e585
SidenkoVitaliy <vitalk_0@qip.ru>
2012-06-16 00:23:04 +0300

    comments added (Task3(15))

--- Task3/Task3_15/main.cpp
+++ Task3/Task3_15/main.cpp
@@ -1,9 +1,21 @@
+/* 
+ * Create a function that changes in a given character string all words, 
+ * containing the specified letter, into relevant number of '*' characters. 
+ * Using created function "hide" all the words in sentences, which meets a given letter.
+ */
+
 #include <iostream>
 #define MAX_LEN 200
 
 using namespace std;
 
-void hideWords(char *str, char c);
+/**
+  * @brief	Function changes all words, containing given letter into asterisks
+  * @param	[in,out]    pStr Pointer to a string ending with '\0'
+  * @param	[in]        c Letter to search in words
+  * @return	void
+  */
+void hideWords(char *pStr, char c);
 
 int main()
 {
@@ -36,21 +48,26 @@ void hideWords(char *pStr, char c)
 	if(!pStr)
 		return;
 
+	// Loop until the end of string
 	while(*pStr)
 	{
+		// Beginning of a word found
 		if(*pStr != ' ')
 		{
 			char *pStart = pStr;
 			bool hide = false;
 
+			// Loop to find the end of a word
 			while(*pStr && (*pStr != ' '))
 			{
+				// Check if word contains a character
 				if(*pStr == c)
 					hide = true;
 
 				pStr++;
 			}
 			
+			// If word contained given character, fill it with asterisks
 			if(hide)
 				memset(pStart, '*', pStr - pStart); 
 		}
